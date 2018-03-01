7b1db5fd670a49c06067635d1d7408bb0d52a175
Lucas <lucas.caidd@gmail.com>
2018-03-01 16:23:54 +0800

    MaterialDesign DialogHost Focus

---
+++ WpfApp7/Extensions/FocusBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+using System.Windows.Controls;
+
+namespace WpfApp7.Extensions
+{
+    public class FocusBehavior : Behavior<Button>
+    {
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.IsVisibleChanged += AssociatedObjectOnIsVisibleChanged;
+
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.IsVisibleChanged -= AssociatedObjectOnIsVisibleChanged;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObjectOnIsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+            AssociatedObject.Focus();
+        }
+    }
+}
---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HiddenPannelVisibilityButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenCancelPanel.IsEnabled = true;
+            HiddenPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void HiddenPanelCancel_Click(object sender, RoutedEventArgs e)
+        {
+            OpenCancelPanel.IsEnabled = true;
+            HiddenPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void OpenHiddenPanel_Click(object sender, RoutedEventArgs e)
+        {
+            HiddenPanel.Visibility = Visibility.Visible;
+            OpenCancelPanel.IsEnabled = false;
+        }
+    }
+}
